a69139056e4bb146b65e7576feefe30a033fd6b0
mattjay1024 <mattjay.abraham@gmail.com>
2017-03-10 11:18:25 -0500

    Added gps node

---
+++ gps/src/gps_node.cpp
@@ -0,0 +1,282 @@
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <vn/sensors/sensors.h>
+#include <boost/bind.hpp>
+#include "ThreadPool.h"
+
+#include <gps/Heading.h>
+#include <gps/Position.h>
+#include <gps/Status.h>
+
+// | 0  |  1  |  2  |  3  |  4 |  5  |  6  |  7  |  8  |  9  |  10  |  11  |  12  |  13  |  14  |  15  |
+// | INS MODE |  ^  |     SENSOR ERROR     | N/A |  ^  |  ^  |                   N/A                   |       
+//            | GPS FIX |                        |  ^  | GPS COMPASS |
+//                                               | GPS HEADING INS |
+
+// Bit masks to help extract the status info
+
+#define INS_MODE_MASK 0x3
+#define GPS_FIX_MASK 0x4
+#define IMU_ERROR_MASK 0x10
+#define MAGPRES_ERROR_MASK 0x20
+#define GPS_ERROR_MASK 0x40
+#define GPS_HEADING_INS_MASK 0x100
+#define GPS_COMPASS_MASK 0x200
+
+// class vn300_node
+// Description: interfaces with vectornav sensor via vnproglib and outputs ros messages
+
+class vn300_node {
+	friend void vn300_packet_handler(void *userdata, vn::protocol::uart::Packet &p, size_t index);
+	friend void vn300_error_handler(void *userdata, vn::protocol::uart::Packet &e, size_t index);
+	friend std::string cookSensorError(vn::protocol::uart::SensorError e);
+
+	vn::sensors::VnSensor sensor;
+
+	nbsdx::concurrent::ThreadPool<5> workers;
+
+	ros::Publisher heading;
+	ros::Publisher position;
+	ros::Publisher status;
+
+	ros::NodeHandle node;
+
+	std::string device;
+	int rate;
+
+	void setup();
+
+	public:
+		vn300_node();
+		~vn300_node() {
+			sensor.disconnect();
+			workers.JoinAll();
+		};
+
+		bool ok() { return sensor.isConnected(); };
+
+		void publish_heading_wrapper(gps::Heading msg) { heading.publish(msg); };
+		void publish_position_wrapper(gps::Position msg) { position.publish(msg); };
+		void publish_status_wrapper(gps::Status msg) { status.publish(msg); };
+
+		unsigned published() { return workers.JobsRemaining(); };
+};
+
+std::string cookSensorError(vn::protocol::uart::SensorError e) {
+	using namespace vn::protocol::uart;	
+	switch(e) {
+		case ERR_HARD_FAULT:
+			return "HARDWARE FAULT";
+		break;
+		case ERR_SERIAL_BUFFER_OVERFLOW:
+			return "SERIAL BUFFER OVERFLOW";
+		break;
+		case ERR_INVALID_CHECKSUM:
+			return "INVALID CHECKSUM";
+		break;          
+		case ERR_INVALID_COMMAND:
+			return "INVALID COMMAND";
+		break;          
+		case ERR_NOT_ENOUGH_PARAMETERS:
+			return "NOT ENOUGH PARAMETERS";
+		break;
+		case ERR_TOO_MANY_PARAMETERS:
+			return "TOO MANY PARAMETERS";
+		break;       
+		case ERR_INVALID_PARAMETER:
+			return "INVALID PARAMETER";
+		break;    
+		case ERR_INVALID_REGISTER:
+			return "INVALID REGISTER";
+		break; 
+		case ERR_UNAUTHORIZED_ACCESS:
+			return "UNAUTHORIZED ACCESS";
+		break; 
+		case ERR_WATCHDOG_RESET:
+			return "WATCHDOG RESET";
+		break;   
+		case ERR_OUTPUT_BUFFER_OVERFLOW:
+			return "OUTPUT BUFFER OVERFLOW";
+		break;   
+		case ERR_INSUFFICIENT_BAUD_RATE:
+			return "INSUFFICIENT BAUD RATE";
+		break;
+		case ERR_ERROR_BUFFER_OVERFLOW:
+			return "ERROR BUFFER OVERFLOW";
+		break;
+		default:
+			return "What error?";
+	}
+}
+
+void vn300_packet_handler(void *userdata, vn::protocol::uart::Packet &p, size_t index) {
+	using namespace vn::sensors;
+	using namespace vn::math;
+	using namespace vn::protocol::uart;
+
+	vn300_node *obj = (vn300_node *)userdata;
+
+	if(p.type() == Packet::TYPE_BINARY) {
+		ROS_INFO_THROTTLE(2, "Binary packet recevied");
+		if(p.isCompatible(COMMONGROUP_YAWPITCHROLL, TIMEGROUP_NONE, IMUGROUP_NONE, GPSGROUP_NONE, ATTITUDEGROUP_YPRU, INSGROUP_NONE)) {	
+			// p is a heading packet
+			gps::Heading msg;
+
+			msg.header.stamp = ros::Time::now();
+
+			vec3f ypr = p.extractVec3f();
+			vec3f ypr_u = p.extractVec3f();
+			
+			for(int i = 0; i < 3; i++) {
+				msg.heading[i] = ypr[i];
+				msg.heading[i + 3] = ypr_u[i];
+			}
+
+			obj->workers.AddJob(boost::bind(&vn300_node::publish_heading_wrapper, obj, msg));
+
+		} else if(p.isCompatible(COMMONGROUP_ANGULARRATE | COMMONGROUP_POSITION | COMMONGROUP_VELOCITY, TIMEGROUP_NONE, IMUGROUP_NONE, GPSGROUP_POSU | GPSGROUP_VELU, ATTITUDEGROUP_NONE, INSGROUP_NONE)) {			
+			// p is a position packet
+			gps::Position msg;
+				
+			msg.header.stamp = ros::Time::now();
+		
+			vec3f angular_rate = p.extractVec3f();
+			vec3d pos_lla = p.extractVec3d();
+			vec3f vel_ned = p.extractVec3f();
+			vec3f pos_u = p.extractVec3f();
+			float vel_u = p.extractFloat();
+
+			msg.velocity[3] = vel_u;
+
+			for(int i = 0; i < 3; i++) {
+				msg.velocity[i] = vel_ned[i];
+				msg.angular[i] = angular_rate[i];
+				msg.position[i] = pos_lla[i];
+				msg.position[i + 3] = pos_u[i];
+			}
+
+			obj->workers.AddJob(boost::bind(&vn300_node::publish_position_wrapper, obj, msg));
+
+		} else if(p.isCompatible(COMMONGROUP_INSSTATUS, TIMEGROUP_NONE, IMUGROUP_NONE, GPSGROUP_NUMSATS | GPSGROUP_FIX, ATTITUDEGROUP_NONE, INSGROUP_NONE)) {			
+			// p is a status packet
+			gps::Status msg;
+
+			msg.header.stamp = ros::Time::now();
+		
+			uint16_t ins_stat = p.extractUint16();
+			uint8_t gps_num_sats = p.extractUint8();
+			uint8_t gps_fix = p.extractUint8();
+
+			msg.ins_mode = (ins_stat & INS_MODE_MASK);
+			msg.usingGPSHeading = (bool)(ins_stat & GPS_HEADING_INS_MASK);
+			msg.gpsCompassActive = (bool)(ins_stat & GPS_COMPASS_MASK);
+			msg.imu_error = (bool)(ins_stat & IMU_ERROR_MASK);
+			msg.magpres_error = (bool)(ins_stat & MAGPRES_ERROR_MASK);
+			msg.num_sats = gps_num_sats;
+			msg.fix = gps_fix;
+			msg.gps_error = (bool)(ins_stat & GPS_ERROR_MASK);
+
+			obj->workers.AddJob(boost::bind(&vn300_node::publish_status_wrapper, obj, msg));
+			
+		} else {
+			ROS_INFO("Unknown packet found");
+		}
+	} else {
+		ROS_DEBUG("Ascii packet received");
+	}
+}
+
+void vn300_error_handler(void *userdata, vn::protocol::uart::Packet &e, size_t index) {
+	ROS_INFO("%s", cookSensorError(e.parseError()).c_str());
+}
+
+void vn300_node::setup() {
+	using namespace vn::sensors;
+	using namespace vn::protocol::uart;
+
+	BinaryOutputRegister heading(
+		ASYNCMODE_PORT1,
+		40,
+		COMMONGROUP_YAWPITCHROLL,
+		TIMEGROUP_NONE,
+		IMUGROUP_NONE,
+		GPSGROUP_NONE,
+		ATTITUDEGROUP_YPRU,
+		INSGROUP_NONE			
+	);
+
+	BinaryOutputRegister position(
+		ASYNCMODE_PORT1,
+		50,
+		COMMONGROUP_ANGULARRATE | COMMONGROUP_POSITION | COMMONGROUP_VELOCITY,
+		TIMEGROUP_NONE,
+		IMUGROUP_NONE,
+		GPSGROUP_POSU | GPSGROUP_VELU,
+		ATTITUDEGROUP_NONE,
+		INSGROUP_NONE
+	);
+
+	BinaryOutputRegister status(
+		ASYNCMODE_PORT1,
+		20,
+		COMMONGROUP_INSSTATUS,
+		TIMEGROUP_NONE,
+		IMUGROUP_NONE,
+		GPSGROUP_NUMSATS | GPSGROUP_FIX,
+		ATTITUDEGROUP_NONE,
+		INSGROUP_NONE
+	);
+
+	sensor.writeAsyncDataOutputType(VNOFF);
+
+	ROS_INFO("Connecting binary output 1 . . . ");
+	sensor.writeBinaryOutput1(heading);
+
+	ROS_INFO("Connecting binary output 2 . . . ");
+	sensor.writeBinaryOutput2(position);
+
+	ROS_INFO("Connecting binary output 3 . . . ");
+	sensor.writeBinaryOutput3(status);
+
+	sensor.registerAsyncPacketReceivedHandler(this, vn300_packet_handler);
+	sensor.registerErrorPacketReceivedHandler(NULL, vn300_error_handler);
+}
+
+vn300_node::vn300_node() :
+	device("/dev/ttyUSB0"),
+	rate(115200)
+{
+	using namespace vn::sensors;
+	using namespace vn::protocol::uart;
+
+	// params
+	node.param("device", device, device);
+	node.param("rate", rate, rate);
+	
+	// setup
+	
+	try {
+		sensor.connect(device, rate);
+	} catch(vn::not_found &e) {}
+
+	ROS_INFO("Connected to %s at %d baud", device.c_str(), rate);
+
+	heading = node.advertise< gps::Heading >("heading", true);
+	position = node.advertise< gps::Position >("position", true);
+	status = node.advertise< gps::Status >("status", true);
+
+	setup();
+}
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "vn300_node");
+	vn300_node gps;
+	ros::Rate r(10);
+
+	while(ros::ok() && gps.ok()) { 
+		ROS_INFO("Publishing jobs remaining: %d", gps.published());
+		r.sleep(); 
+	}
+
+	return 0;	
+}
