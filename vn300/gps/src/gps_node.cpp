d3ec314e1969399fae69a1351f24ba6cf4cad287
mattjay1024 <mattjay.abraham@gmail.com>
2017-05-12 14:11:29 -0400

    Renamed node

--- vn300/gps/src/gps_node.cpp
+++ vn300/gps/src/gps_node.cpp
@@ -4,9 +4,9 @@
 #include <boost/bind.hpp>
 #include "ThreadPool.h"
 
-#include <gps/Heading.h>
-#include <gps/Position.h>
-#include <gps/Status.h>
+#include <vn300/Heading.h>
+#include <vn300/Position.h>
+#include <vn300/Status.h>
 
 // | 0  |  1  |  2  |  3  |  4 |  5  |  6  |  7  |  8  |  9  |  10  |  11  |  12  |  13  |  14  |  15  |
 // | INS MODE |  ^  |     SENSOR ERROR     | N/A |  ^  |  ^  |                   N/A                   |       
@@ -24,27 +24,31 @@
 #define GPS_COMPASS_MASK 0x200
 
 // class vn300_node
-// Description: interfaces with vectornav sensor via vnproglib and outputs ros messages
+// Description: interfaces with vectornav sensor via vnproglib and outputs ros messages as they come in
 
 class vn300_node {
-	friend void vn300_packet_handler(void *userdata, vn::protocol::uart::Packet &p, size_t index);
-	friend void vn300_error_handler(void *userdata, vn::protocol::uart::Packet &e, size_t index);
-	friend std::string cookSensorError(vn::protocol::uart::SensorError e);
+	private:
+		// vnproglib makes it impossible to use a class member as a packet handler easily.
+		// so all these functions are friends to have access to the private members of a vn300_node but remain outside class
+		// may use boost::bind at some point to get around that
+		friend void vn300_packet_handler(void *userdata, vn::protocol::uart::Packet &p, size_t index);
+		friend void vn300_error_handler(void *userdata, vn::protocol::uart::Packet &e, size_t index);
+		friend std::string cookSensorError(vn::protocol::uart::SensorError e);
 
-	vn::sensors::VnSensor sensor;
+		vn::sensors::VnSensor sensor;
 
-	nbsdx::concurrent::ThreadPool<5> workers;
+		nbsdx::concurrent::ThreadPool<5> workers; // 5 worker threads so we don't get a slowdown from serializing all the messages we get
 
-	ros::Publisher heading;
-	ros::Publisher position;
-	ros::Publisher status;
+		ros::Publisher heading;
+		ros::Publisher position;
+		ros::Publisher status;
 
-	ros::NodeHandle node;
+		ros::NodeHandle node;
 
-	std::string device;
-	int rate;
+		std::string device;
+		int rate;
 
-	void setup();
+		void setup();
 
 	public:
 		vn300_node();
@@ -55,12 +59,21 @@ class vn300_node {
 
 		bool ok() { return sensor.isConnected(); };
 
-		void publish_heading_wrapper(gps::Heading msg) { heading.publish(msg); };
-		void publish_position_wrapper(gps::Position msg) { position.publish(msg); };
-		void publish_status_wrapper(gps::Status msg) { status.publish(msg); };
+		// publishing wrappers allow us to add the message serialization to the thread pool
 
-		unsigned published() { return workers.JobsRemaining(); };
+		void publish_heading_wrapper(vn300::Heading msg) { heading.publish(msg); };
+		void publish_position_wrapper(vn300::Position msg) { position.publish(msg); };
+		void publish_status_wrapper(vn300::Status msg) { status.publish(msg); };
+
+		unsigned published() { return workers.JobsRemaining(); }; // debug function
 };
+
+/*
+	cookSensorError
+	parameters:
+		e - integer representing the sensor error
+	return: error message string
+*/
 
 std::string cookSensorError(vn::protocol::uart::SensorError e) {
 	using namespace vn::protocol::uart;	
@@ -109,18 +122,28 @@ std::string cookSensorError(vn::protocol::uart::SensorError e) {
 	}
 }
 
+/* 
+	vn300_packet_handler
+	parameters:
+		userdata - (stores pointer to vn300_node object)
+		p - the packet received from the sensor
+		index - packet count so far
+	description:
+		receives binary packets from sensor, extracts data, and sends job to thread pool to serialize the message and send it.
+*/
+
 void vn300_packet_handler(void *userdata, vn::protocol::uart::Packet &p, size_t index) {
 	using namespace vn::sensors;
 	using namespace vn::math;
 	using namespace vn::protocol::uart;
 
-	vn300_node *obj = (vn300_node *)userdata;
+	vn300_node *obj = (vn300_node *)userdata; // nasty, but it'll do
 
 	if(p.type() == Packet::TYPE_BINARY) {
 		ROS_INFO_THROTTLE(2, "Binary packet recevied");
 		if(p.isCompatible(COMMONGROUP_YAWPITCHROLL, TIMEGROUP_NONE, IMUGROUP_NONE, GPSGROUP_NONE, ATTITUDEGROUP_YPRU, INSGROUP_NONE)) {	
 			// p is a heading packet
-			gps::Heading msg;
+			vn300::Heading msg;
 
 			msg.header.stamp = ros::Time::now();
 
@@ -136,7 +159,7 @@ void vn300_packet_handler(void *userdata, vn::protocol::uart::Packet &p, size_t 
 
 		} else if(p.isCompatible(COMMONGROUP_ANGULARRATE | COMMONGROUP_POSITION | COMMONGROUP_VELOCITY, TIMEGROUP_NONE, IMUGROUP_NONE, GPSGROUP_POSU | GPSGROUP_VELU, ATTITUDEGROUP_NONE, INSGROUP_NONE)) {			
 			// p is a position packet
-			gps::Position msg;
+			vn300::Position msg;
 				
 			msg.header.stamp = ros::Time::now();
 		
@@ -144,7 +167,7 @@ void vn300_packet_handler(void *userdata, vn::protocol::uart::Packet &p, size_t 
 			vec3d pos_lla = p.extractVec3d();
 			vec3f vel_ned = p.extractVec3f();
 			vec3f pos_u = p.extractVec3f();
-			float vel_u = p.extractFloat();
+			float vel_u = p.extractFloat(); // note vel_u field in GPS group is NOT a vector of 3 floats, but a single float. attempting to extract a vec3f will cause the program to become nonresponsive.
 
 			msg.velocity[3] = vel_u;
 
@@ -159,13 +182,15 @@ void vn300_packet_handler(void *userdata, vn::protocol::uart::Packet &p, size_t 
 
 		} else if(p.isCompatible(COMMONGROUP_INSSTATUS, TIMEGROUP_NONE, IMUGROUP_NONE, GPSGROUP_NUMSATS | GPSGROUP_FIX, ATTITUDEGROUP_NONE, INSGROUP_NONE)) {			
 			// p is a status packet
-			gps::Status msg;
+			vn300::Status msg;
 
 			msg.header.stamp = ros::Time::now();
 		
 			uint16_t ins_stat = p.extractUint16();
 			uint8_t gps_num_sats = p.extractUint8();
 			uint8_t gps_fix = p.extractUint8();
+
+			// ins status bit field can be found at top of program
 
 			msg.ins_mode = (ins_stat & INS_MODE_MASK);
 			msg.usingGPSHeading = (bool)(ins_stat & GPS_HEADING_INS_MASK);
@@ -186,9 +211,21 @@ void vn300_packet_handler(void *userdata, vn::protocol::uart::Packet &p, size_t 
 	}
 }
 
+/*
+	vn300_error_handler
+	parameters:	
+		read vn300_packet_handler
+*/
+
 void vn300_error_handler(void *userdata, vn::protocol::uart::Packet &e, size_t index) {
 	ROS_INFO("%s", cookSensorError(e.parseError()).c_str());
 }
+
+/*
+	setup
+	description: 
+		sets up the binary registers to send out packets at at 10, 8, and 20 hz respectively. also registers the packet and error handlers with the library.
+*/
 
 void vn300_node::setup() {
 	using namespace vn::sensors;
@@ -242,6 +279,12 @@ void vn300_node::setup() {
 	sensor.registerErrorPacketReceivedHandler(NULL, vn300_error_handler);
 }
 
+/* 
+	constructor
+	description:
+		attempts to connect to sensor. if sensor is not connected, throws vn::not_found. also advertises heading, position, and status.
+*/
+
 vn300_node::vn300_node() :
 	device("/dev/ttyUSB0"),
 	rate(115200)
@@ -250,20 +293,20 @@ vn300_node::vn300_node() :
 	using namespace vn::protocol::uart;
 
 	// params
-	node.param("device", device, device);
+	node.param("device", device, device); // for roslaunch files
 	node.param("rate", rate, rate);
 	
 	// setup
 	
 	try {
-		sensor.connect(device, rate);
+		sensor.connect(device, rate); // tried to prevent it from aborting, but couldn't find a workaround.
 	} catch(vn::not_found &e) {}
 
 	ROS_INFO("Connected to %s at %d baud", device.c_str(), rate);
 
-	heading = node.advertise< gps::Heading >("heading", true);
-	position = node.advertise< gps::Position >("position", true);
-	status = node.advertise< gps::Status >("status", true);
+	heading = node.advertise< vn300::Heading >("heading", true);
+	position = node.advertise< vn300::Position >("position", true);
+	status = node.advertise< vn300::Status >("status", true);
 
 	setup();
 }
@@ -274,7 +317,7 @@ int main(int argc, char **argv) {
 	ros::Rate r(10);
 
 	while(ros::ok() && gps.ok()) { 
-		ROS_INFO("Publishing jobs remaining: %d", gps.published());
+		//ROS_INFO("Publishing jobs remaining: %d", gps.published()); // debug
 		r.sleep(); 
 	}
 
