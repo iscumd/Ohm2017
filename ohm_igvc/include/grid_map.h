15f89d7594f403f85cd1bc652626f482a0a0efe0
mattjay1024 <mattjay.abraham@gmail.com>
2017-06-02 23:29:54 -0400

    Fixed errors and updated CMakeLists

--- ohm_igvc/include/grid_map.h
+++ ohm_igvc/include/grid_map.h
@@ -12,10 +12,17 @@
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/Point32.h>
 #include <geometry_msgs/Quaternion.h>
+#include <ohm_igvc/get_successors.h>
+#include <ohm_igvc/cell_to_real.h>
+#include <ohm_igvc/real_to_cell.h>
+#include <ohm_igvc/robot_position.h>
+#include <ohm_igvc/pixel_locations.h>
 
 // opencv includes
 
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 
 // stl includes
 
@@ -25,15 +32,15 @@
 
 class grid_map {
 	public:
-		grid_map(int width, int height, double resolution, int threshold, geometry_msgs::Point raster_reference); // 
+		grid_map(unsigned short threshold); // 
 		void laser_scan_update(const sensor_msgs::LaserScan::ConstPtr &scan); //  
 		void point_cloud_update(const sensor_msgs::PointCloud::ConstPtr &points); // 
 		void camera_update(const ohm_igvc::pixel_locations::ConstPtr &cam);
-		void odometry_update(const nav_msgs::Odometry::ConstPtr &odom); // 
+		void odometry_update(const geometry_msgs::Pose2D::ConstPtr &odom); // 
 		void display_map(const ros::TimerEvent &e);
 
 	private:
-		bool successors_callback(ohm_igvc::successors::Request &rq, ohm_igvc::successors::Response &rp); // 
+		bool successors_callback(ohm_igvc::get_successors::Request &rq, ohm_igvc::get_successors::Response &rp); // 
 		bool cell_to_real_callback(ohm_igvc::cell_to_real::Request &rq, ohm_igvc::cell_to_real::Response &rp); //
 		bool real_to_cell_callback(ohm_igvc::real_to_cell::Request &rq, ohm_igvc::real_to_cell::Response &rp); //
 		bool robot_position_callback(ohm_igvc::robot_position::Request &rq, ohm_igvc::robot_position::Response &rp); //
@@ -47,8 +54,8 @@ class grid_map {
 		geometry_msgs::Pose2D odometry;
 
 		// map size
-		const double m_height, m_width, m_resolution;
-		const int m_grid_x, m_grid_y;
+		double m_height, m_width, m_resolution;
+		int m_grid_x, m_grid_y;
 		geometry_msgs::Point raster_reference;
 
 		// occupancy
